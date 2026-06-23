79dfc2938b725cc8dc0e94855fe91f79aefa59fb
arg50 <arg50@LD>
2026-06-23 18:58:24 +0300

    new 5 (fakestore в app.js)

--- js/app.js
+++ js/app.js
@@ -28,3 +28,56 @@ buttons.forEach(button => {
     alert("Товар додано у кошик");
   });
 });
+
+const container = document.querySelector(".container");
+
+fetch("https://fakestoreapi.com/products/category/electronics")
+  .then(response => response.json())
+  .then(products => {
+
+    products.forEach(product => {
+
+      const card = document.createElement("div");
+      card.classList.add("card");
+
+      const price = Math.round(product.price * 40);
+
+      card.innerHTML = `
+        <img src="${product.image}" alt="${product.title}">
+        <h3>${product.title}</h3>
+        <p class="price">${price} грн</p>
+        <button>У кошик</button>
+      `;
+
+      const button = card.querySelector("button");
+
+      button.addEventListener("click", () => {
+
+        let cart =
+          JSON.parse(localStorage.getItem("cart")) || [];
+
+        cart.push({
+          name: product.title,
+          price: `${price} грн`
+        });
+
+        localStorage.setItem(
+          "cart",
+          JSON.stringify(cart)
+        );
+
+        document.cookie =
+          "cart=" +
+          encodeURIComponent(JSON.stringify(cart)) +
+          "; max-age=604800; path=/";
+
+        alert("Товар додано у кошик");
+      });
+
+      container.append(card);
+    });
+
+  })
+  .catch(error => {
+    console.error("Помилка:", error);
+  });
