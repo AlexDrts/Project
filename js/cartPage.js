e69d3d195ec4699cc465688110cfc50348cee924
arg50 <arg50@LD>
2026-06-23 17:45:30 +0300

    new 2

--- js/cartPage.js
+++ js/cartPage.js
@@ -20,7 +20,57 @@ cart.forEach(item => {
 
   cartItems.append(div);
 
-  sum += parseInt(item.price);
+  const price = Number(
+    item.price.replace(/\s/g, "").replace("грн", "")
+  );
+
+  sum += price;
 });
 
 total.textContent = `Разом: ${sum} грн`;
+
+const form = document.getElementById("orderForm");
+
+form.addEventListener("submit", function(event) {
+
+  event.preventDefault();
+
+  const name = document.getElementById("name").value.trim();
+  const phone = document.getElementById("phone").value.trim();
+  const email = document.getElementById("email").value.trim();
+
+  if (name.length < 2) {
+    alert("Введіть коректне ім'я");
+    return;
+  }
+
+  if (phone.length < 10) {
+    alert("Введіть коректний телефон");
+    return;
+  }
+
+  if (!email.includes("@")) {
+    alert("Введіть коректний email");
+    return;
+  }
+
+  const orderData = {
+    name,
+    phone,
+    email,
+    city: document.getElementById("city").value,
+    delivery:
+    document.querySelector(
+      'input[name="delivery"]:checked'
+    ).value,
+    gift:
+    document.getElementById("gift").checked,
+    insurance:
+    document.getElementById("insurance").checked
+  };
+
+  console.log("Замовлення:");
+  console.log(orderData);
+
+  alert("Замовлення успішно оформлено!");
+});
